3096d42d175af2322db868108309a0f25dd18905
matt9874 <mattcave623@gmail.com>
2020-03-10 22:55:05 +0000

    Validate input in main method

--- HackerNewsScraper/Program.cs
+++ HackerNewsScraper/Program.cs
@@ -1,11 +1,26 @@
-﻿namespace HackerNewsScraper
+﻿using HackerNewsScraper.InputValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace HackerNewsScraper
 {
     class Program
     {
         static void Main(string[] args)
         {
             var serviceProvider = DependencyInjectionConfiguration.GetServiceProvider();
-            
+
+            IInputValidator inputValidator = serviceProvider.GetRequiredService<IInputValidator>();
+            InputStatus inputStatus = inputValidator.Validate(args);
+
+            if (inputStatus.IsValid)
+            {
+
+            }
+            else 
+            {
+                Console.WriteLine(inputStatus.ErrorMessage);
+            }
         }
     }
 }
